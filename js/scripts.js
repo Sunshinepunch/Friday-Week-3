32f3682ae242d8050cd719170189c4a51cbdd350
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-08-20 11:36:43 -0700

    return a set of numbers up to the number input by the user

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,18 @@ function roboRog (nombre) {
   let string = nombre.replace(/\D/gi,"NaN. Try again.")
   if (string.includes("NaN")) {
     return (nombre + " is not a number. Try again friend.")
-    } else {
-    return nombre;
+    }
+  let numeral = parseFloat(string) + 1;
+  let arrayNum = []
+  let counter = 0
+  for(i=0; i <= numeral; i++) {
+    if(counter < numeral) {
+      arrayNum.push(counter);
+      counter++;
+      console.log(arrayNum);
   }
+}
+  return arrayNum;
 }
 
 
