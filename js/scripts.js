660685491d8a5dfbedbdd2aeb8a0214b4013cbe8
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-08-22 10:08:14 -0700

    display updated read.me with tests, no longer display nonfunctional regex code, display working behaviors

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,24 @@
 //Business Logic
 
 function roboRog (nombre) {
-  let string = nombre.replace(/\D/gi,"NaN. Try again.")
+  let string = nombre.replace(/\D/gi,"NaN.")
+  let arrayNum = [];
   if (string.includes("NaN")) {
     return (nombre + " is not a number. Try again friend.")
     }
-  let numeral = parseFloat(string) + 1;
-  let arrayNum = []
-  let counter = 0
-  for(i=0; i <= numeral; i++) {
-    if(counter < numeral) {
-      arrayNum.push(counter);
-      counter++;
-  }
-} console.log(arrayNum)
-  for(index=0; index <= numeral; index++) {
-    if (index.toString().includes(3)){
-      arrayNum.toString().replace(/[3]/gi,'"Won\'t you be my neighbor?"')
-      console.log(arrayNum);
-    } else if(index.toString().includes(2)) {
-      arrayNum.toString().replace(/[2]/gi,'Boop!')
-    } else if(index.toString().includes(1)) {
-      arrayNum.toString().replace(/[1]/gi,'Beep!')
+  for (i = 0; i <= nombre; i++) {
+    if (i.toString().includes(3)) {
+      arrayNum.push("Wont you be my neighbor?");
+    } else if (i.toString().includes(2)) {
+      arrayNum.push("Boop!")
+    } else if (i.toString().includes(1)) {
+      arrayNum.push("Beep!")
     } else {
-      arrayNum.push(index)
+      arrayNum.push(i);
     }
   }
   return arrayNum;
 };
-
-// works but doesnt use the regex I want
-// let arrayNum = [];
-// for (i = 0; i <= number; i++) {
-//   if (i.toString().includes(3)) {
-//     arrayNum.push("Wont you be my neighbor?");
-//   } else if (i.toString().includes(2)) {
-//     arrayNum.push("boop!")
-//   } else if (i.toString().includes(1)) {
-//     arrayNum.push("Beep!")
-//   } else {
-//     arrayNum.push(i);
-//   }
-
-// }
-// return output;
-// };
 
 
 
