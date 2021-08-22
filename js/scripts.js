de158edf1652bcafe1cb5a74f507b08482a53e57
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-08-22 10:24:11 -0700

    display correct formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,25 @@
 //Business Logic
 
-function roboRog (nombre) {
-  let string = nombre.replace(/\D/gi,"NaN.")
+function roboRog(nombre) {
+  let string = nombre.replace(/\D/gi, "NaN.");
   let arrayNum = [];
   if (string.includes("NaN")) {
-    return (nombre + " is not a number. Try again friend.")
-    }
+    return nombre + " is not a number. Try again friend.";
+  }
+
   for (i = 0; i <= nombre; i++) {
     if (i.toString().includes(3)) {
       arrayNum.push("Wont you be my neighbor?");
     } else if (i.toString().includes(2)) {
-      arrayNum.push("Boop!")
+      arrayNum.push("Boop!");
     } else if (i.toString().includes(1)) {
-      arrayNum.push("Beep!")
+      arrayNum.push("Beep!");
     } else {
       arrayNum.push(i);
     }
   }
   return arrayNum;
-};
-
-
-
-
-
+}
 
 //ui logic
 $(document).ready(function () {
